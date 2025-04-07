715ea6e2d1b2d32a9dc9e58b8eb856624ccd106c
nikhildby <115280806+nikhildby@users.noreply.github.com>
2025-04-07 10:14:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file).convert('RGB')
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
 
     # Preprocess the image
     image = image.resize((32, 32))
